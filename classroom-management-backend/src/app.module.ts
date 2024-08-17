16a8325d6dbc21f6455304ad346060ba43147fec
kishlaykiku <kishlaykumarkkp24@gmail.com>
2024-08-18 02:01:54 +0530

    backend-tested-200-ok

--- classroom-management-backend/src/app.module.ts
+++ classroom-management-backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
@@ -10,13 +10,14 @@ import { ClassroomsModule } from './classrooms/classrooms.module';
 import { TimetablesModule } from './timetables/timetables.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),
-            MongooseModule.forRoot(process.env.MONGODB_URI),
-            UsersModule,
-            ClassroomsModule,
-            TimetablesModule,
-          ],
-  // controllers: [AppController],
-  // providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGODB_URI),
+        UsersModule,
+        ClassroomsModule,
+        TimetablesModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- classroom-management-backend/src/users/users.service.ts
+++ classroom-management-backend/src/users/users.service.ts
@@ -7,23 +7,28 @@ import { User, UserDocument } from './user.schema';
 export class UsersService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+    // Create a new user
     async createUser(userDto: any): Promise<User> {
         const newUser = new this.userModel(userDto);
         return newUser.save();
     }
 
+    // Find all existing users
     async findAll(): Promise<User[]> {
         return this.userModel.find().exec();
     }
 
+    // Find a user by ID
     async findOne(id: string): Promise<User> {
         return this.userModel.findById(id).exec();
     }
 
+    // Update a user by ID
     async updateUser(id: string, userDto: any): Promise<User> {
         return this.userModel.findByIdAndUpdate(id, userDto, { new: true }).exec();
     }
 
+    // Delete a user by ID
     async deleteUser(id: string): Promise<User> {
         return this.userModel.findByIdAndDelete(id).exec();
     }
